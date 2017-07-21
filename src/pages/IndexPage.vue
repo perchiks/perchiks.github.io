3afed7f2ba3d7eb5b9489ba5d01e6380dc652975
Stanislav Mihailov <stanislav.mihailov@maximumtest.ru>
2017-07-21 12:41:23 +0300

    fixed mobile

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,47 @@
 <template>
-    <container class="container" align="center center">
-        <column size="4">
+    <div class="container">
+        <div class="column">
             <figure>
                 <img src="/dist/img/logo.svg">
                 <figcaption><h1>Скоро здесь будет горячо</h1></figcaption>
             </figure>
-        </column>
-    </container>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped="scoped">
     @import '../styles/_colors.scss';
+    @import '../styles/_mq.scss';
     .container {
         height: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-basis: 100%;
+        align-items: center;
+        justify-content: center;
+        .column {
+            display: flex;
+            flex-direction: column;
+            flex-basis: 100/12*4%;
+            @include media($small) {
+                flex-basis: 80%;
+                h1 {
+                    font-size: 4rem;
+                }
+            }
+            @include media($medium) {
+                flex-basis: 80%;
+                h1 {
+                    font-size: 4rem;
+                }
+            }
+            @include media($large) {
+                flex-basis: 100/12*4%;
+                h1 {
+                    font-size: 2.5rem;
+                }
+            }
+        }
     }
     figure {
         margin: 0;
