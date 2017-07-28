960de3c7c6e633363868b6e9a8b31c5d9ecb0a36
Stanislav Mihailov <stanislav.mihailov@maximumtest.ru>
2017-07-28 13:07:42 +0300

    add missed action to login button & style

--- src/pages/signin.vue
+++ src/pages/signin.vue
@@ -1,14 +1,50 @@
 <template>
     <article>
         <navigation :title="title"></navigation>
-        <input type="text" v-model="email" placeholder="Login">
-        <input type="password" v-model="password" placeholder="Password">
-        <a class="btn">Sign in</a>
+        <div class="form">
+            <label><input type="text" v-model="email" placeholder="Login"></label>
+            <label><input type="password" v-model="password" placeholder="Password"></label>
+            <label><a class="btn" @click="auth()">Войти</a></label>
+        </div>
     </article>
 </template>
 
 <style lang="scss" scoped="scoped">
-
+    @import "../styles/_colors.scss";
+    article {
+        .form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            flex-basis: 80%;
+            margin-top: 25%;
+        }
+        label {
+            display: inline-flex;
+            flex: 1;
+            align-items: center;
+            justify-content: center;
+            &:not(:first-of-type) {
+                margin-top: 1rem;
+            }
+            input, textarea {
+                width: 100%;
+                border-radius: 4px;
+                font-size: 0.75em;
+                line-height: 1.0625em;
+                border: 1px solid #D3D3D3;
+                padding: .5rem;
+            }
+            a {
+                border: 1px solid $primary;
+                padding: .5rem;
+                border-radius: 4px;
+                color: $primary;
+                text-transform: uppercase;
+            }
+        }
+    }
 </style>
 
 <script>
@@ -30,7 +66,8 @@
         methods: {
             auth() {
                 let self = this;
-                firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(function() {
+                firebase.auth().createUserWithEmailAndPassword(self.email, self.password).then(function() {
+                    console.log('login ok');
                     self.$router.push({ path: '/manage' });
                 }).catch(function(error) {
                     // Handle Errors here.
